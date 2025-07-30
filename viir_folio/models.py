12a3e6c4c576b5561e6169a666cc4a11872b4daf
Jaideep <vinnijaideep@gmail.com>
2025-07-30 19:59:21 +0530

    publication ui

--- viir_folio/models.py
+++ viir_folio/models.py
@@ -43,7 +43,6 @@ class Project(models.Model):
     tech=models.CharField(max_length=100)
     url=models.URLField()
     category=MultiSelectField(max_length=100, choices=(("webdev","Web Dev"),("appdev", "App Dev"), ("graphic","Graphics"),("mlai","ML/AI"),("iot","IoT")), default="webdev", max_choices=5)
-    content=models.CharField(max_length=100)
     image=models.ImageField(default="image.png")
     def __str__(self):
         return self.title
